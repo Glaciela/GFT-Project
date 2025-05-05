d10a4317c75d21ac52d9e2505ed1e3839c402e53
Glaciela Cortez <glaciela@hotmail.com>
2025-05-05 10:31:13 -0400

    Removendo factory.py

--- permission/tests/test_permission_search_view.py
+++ permission/tests/test_permission_search_view.py
@@ -24,3 +24,32 @@ class PermissionSearchViewTest(PermissionTestBase):
             'Busca por &quot;teste&quot;',
             response.content.decode('utf-8')
         )
+    def test_permission_search_can_find_permission_by_location(self):
+        location1 = 'Av Afonso Pena, 1000'
+        location2 = 'Av Afonso Pena, 2000'
+
+        permission1 = self.make_permission(
+            location=location1,
+            slug='slug1',
+            author_data={'username': 'newauthor1'},
+        )
+
+        pemission2 = self.make_permission(
+            location=location2,
+            slug='slug2',
+            author_data={'username': 'newauthor2'},
+        )
+
+        search_url = reverse('permission:search')
+        response1 = self.client.get(f'{search_url}?q={location1}')
+        response2 = self.client.get(f'{search_url}?q={location2}')
+        response_both = self.client.get(f'{search_url}?q=Av Afonso Pena')
+
+        self.assertIn(permission1, response1.context['permissions'])
+        self.assertNotIn(pemission2, response1.context['permissions'])
+        self.assertIn(pemission2, response2.context['permissions'])
+        self.assertNotIn(permission1, response2.context['permissions'])
+        self.assertIn(permission1, response_both.context['permissions'])
+        self.assertIn(pemission2, response_both.context['permissions'])
+        
+
