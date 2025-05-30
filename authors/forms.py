f5c64a45ffe21e0931b0b2bcb02f6ded07c9c848
Glaciela Cortez <glaciela@hotmail.com>
2025-05-30 09:33:49 -0400

    .

--- authors/forms.py
+++ authors/forms.py
@@ -17,6 +17,7 @@ class RegisterForm(forms.ModelForm):
     password2 = forms.CharField(
         label='Repita sua Senha',
         widget=forms.PasswordInput(attrs={'placeholder': 'Repita aqui sua senha'}),
+        error_messages={'required': 'Repita sua senha não pode estar vazia.'},
         # validators=[strong_password],
     )
     class Meta:
@@ -30,20 +31,57 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
-        labels = {
-            'first_name': 'Primeiro Nome',
-            'last_name': 'Sobrenome',
-            'username': 'Nome de Usuário',
-            'email': 'Email',
-            'password': 'Senha',
-        }
-        widgets = {
-            'first_name': forms.TextInput(attrs={'placeholder': 'Digite aqui seu primeiro nome'}),
-            'last_name': forms.TextInput(attrs={'placeholder': 'Digite aqui seu sobrenome'}),
-            'username': forms.TextInput(attrs={'placeholder': 'Digite aqui seu nome de usuário'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Digite aqui seu email'}),
-            'password': forms.PasswordInput(attrs={'placeholder': 'Digite aqui sua senha'}),
-        }
+        #labels = {
+            #'first_name': 'Primeiro Nome',
+            #'last_name': 'Sobrenome',
+            # 'username': 'Nome de Usuário',
+            #'email': 'Email',
+            #'password': 'Senha',
+        #}
+        #widgets = {
+            #'first_name': forms.TextInput(attrs={'placeholder': 'Digite aqui seu primeiro nome'}),
+            #'last_name': forms.TextInput(attrs={'placeholder': 'Digite aqui seu sobrenome'}),
+            # 'username': forms.TextInput(attrs={'placeholder': 'Digite aqui seu nome de usuário'}),
+            #'email': forms.EmailInput(attrs={'placeholder': 'Digite aqui seu email'}),
+            #'password': forms.PasswordInput(attrs={'placeholder': 'Digite aqui sua senha'}),
+        #}
+
+    first_name = forms.CharField(
+        error_messages={'required': 'Primeiro Nome não pode estar vazio.'},
+        widget=forms.TextInput(attrs={'placeholder': 'Digite aqui seu primeiro nome'}),
+        label='Primeiro Nome',
+    )
+
+    last_name = forms.CharField(
+        error_messages={'required': 'Sobrenome não pode estar vazio.'},
+        widget=forms.TextInput(attrs={'placeholder': 'Digite aqui seu sobrenome'}),
+        label='Sobrenome',
+    )
+
+    username = forms.CharField(
+        error_messages={'required': 'Username não pode estar vazio.',
+                        'min_length': 'Username deve ter no minimo 4 caracteres.',
+                        'max_length': 'Username deve ter no maximo 150 caracteres.',
+                        },
+        widget=forms.TextInput(attrs={'placeholder': 'Digite aqui seu nome de usuário'}),
+        help_text='Username deve conter letras, números ou @.+-_ . E seu tamanho entre 4 e 150 caracteres.',
+        label='Nome de Usuário',
+        min_length=4,
+        max_length=150,
+    )
+
+    email = forms.EmailField(
+        error_messages={'required': 'Email não pode estar vazio.'},
+        widget=forms.EmailInput(attrs={'placeholder': 'Digite aqui seu email'}),
+        label='Email',
+    )
+
+    password = forms.CharField(
+        error_messages={'required': 'Senha não pode estar vazia.'},
+        widget=forms.PasswordInput(attrs={'placeholder': 'Digite aqui sua senha'}),
+        label='Senha',
+    )
+
     
     def clean(self):
         cleaned_data = super().clean()
@@ -51,7 +89,12 @@ class RegisterForm(forms.ModelForm):
         password = cleaned_data.get('password')
         password2 = cleaned_data.get('password2')
 
-        if password and password2 and password != password2:
+        if not password or not password2:
+                raise forms.ValidationError({
+                    "password": ''
+                })
+
+        elif password and password2 and password != password2:
             raise forms.ValidationError({
                 "password": 'As senhas não coincidem.'
                 })
--- authors/test/test_author_register_form.py
+++ authors/test/test_author_register_form.py
@@ -1,5 +1,101 @@
-from django.test import TestCase
+from django.test import TestCase as DjangoTestCase
+from unittest import TestCase
+from authors.forms import RegisterForm
+from parameterized import parameterized
+from django.urls import reverse
 
 class AuthorRegisterFormUnitTest(TestCase):
-    def test_first_name_placehoder_is_correct(self):
-        ...
+    @parameterized.expand([
+        ('first_name', 'Primeiro Nome'),
+        ('last_name', 'Sobrenome'),
+        ('username', 'Nome de Usuário'),
+        ('email', 'Email'),
+        ('password', 'Senha'),
+        ('password2', 'Repita sua Senha'),
+    ])
+    def test_filds_labels(self, field, label):
+        form = RegisterForm()
+        current_label = form.fields[field].label
+        self.assertEqual(current_label, label)
+
+    @parameterized.expand([
+        ('first_name', 'Digite aqui seu primeiro nome'),
+        ('last_name', 'Digite aqui seu sobrenome'),
+        ('username', 'Digite aqui seu nome de usuário'),
+        ('email', 'Digite aqui seu email'),
+        ('password', 'Digite aqui sua senha'),
+        ('password2', 'Repita aqui sua senha'),
+    ])
+    def test_filds_placehoder(self, field, placeholder):
+        form = RegisterForm()
+        current_placeholder = form.fields[field].widget.attrs['placeholder']
+        self.assertEqual(current_placeholder, placeholder)
+
+    def test_filds_help_text(self):
+        form = RegisterForm()
+        current_help_text = form.fields['username'].help_text
+        self.assertEqual(current_help_text, 'Username deve conter letras, números ou @.+-_ . E seu tamanho entre 4 e 150 caracteres.')
+
+
+class AuthorRegisterFormIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs):
+        self.form_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'HdD0g@example.com',
+            'password': '12345678aA',
+            'password2': '12345678aA',
+        }
+        return super().setUp(*args, **kwargs)
+    
+    @parameterized.expand([
+        ('first_name', 'Primeiro Nome não pode estar vazio.'),
+        ('last_name', 'Sobrenome não pode estar vazio.'),
+        ('username', 'Username não pode estar vazio.'),
+        ('email', 'Email não pode estar vazio.'),
+        ('password', 'Senha não pode estar vazia.'),
+        ('password2', 'Repita sua senha não pode estar vazia.'),
+    ])
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get(field))
+
+    def test_username_field_min_lenght_shoud_be_4(self):
+        self.form_data['username'] = 'joh'
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        msg = 'Username deve ter no minimo 4 caracteres.'
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('username'))
+
+    def test_username_field_max_lenght_shoud_be_150(self):
+        self.form_data['username'] = 'A' * 151
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        msg = 'Username deve ter no maximo 150 caracteres.'
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('username'))
+
+    def test_password_field_is_correct(self):
+        self.form_data['password'] = '123abcA'
+        self.form_data['password2'] = '123abcA'
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        msg = 'A senha deve ter pelo menos 8 caracteres, uma letra minúscula, uma letra maiúscula e um número.'
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('password'))
+
+    def test_password_and_password2_are_equal(self):
+        self.form_data['password'] = '123abcA'
+        self.form_data['password2'] = '123'
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        msg = 'As senhas não coincidem.'
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('password'))
+       
+    
